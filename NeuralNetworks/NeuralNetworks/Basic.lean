b1d4881e99a1aedb416d0c54a1f6b7f64cb4b148
your_github_username <drewsteindl@icloud.com>
2025-03-27 12:08:44 -0400

    Update Basic.lean

--- NeuralNetworks/NeuralNetworks/Basic.lean
+++ NeuralNetworks/NeuralNetworks/Basic.lean
@@ -1,1 +1,16 @@
-def hello := "world"
+import Mathlib
+import Mathlib.Data.Matrix.Notation
+
+open Fin
+open Real
+open Finset BigOperators
+open Matrix
+
+noncomputable def maximum (n k : ℝ) : ℝ :=
+  if n < k then k else n
+
+noncomputable def relu (x : ℝ) : ℝ :=
+  maximum 0.0 x
+
+def affine_function_nd {n : ℕ} (a : Fin n → ℝ) (b : ℝ) (x : Fin n → ℝ) : ℝ :=
+  (∑ i, a i * x i) + b
